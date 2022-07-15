cab4e7e6de7c8e40193247ca3365fc2de2c620bd
jaymalave <jaym.github@gmail.com>
2022-07-15 19:50:10 +0530

    feat: front end setup

--- api/server.js
+++ api/server.js
@@ -4,19 +4,26 @@ import dotenv from "dotenv";
 import router from "./routes/routes.js";
 import bodyParser from "body-parser";
 import connectDB from "./config/db.js";
+import tokenToID from "./middleware/authenticateToken.js";
 
 const app = express();
-
 
 dotenv.config();
 
 app.use(cors());
 
-app.use(express.json());
+app.use(bodyParser.json());
 
 connectDB();
 
-app.use(bodyParser.json());
+app.use(tokenToID);
+
+app.use(express.json());
+
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use("/", router);
+
+app.listen(process.env.PORT || "8000", () => {
+  console.log(`Server running at ${process.env.PORT || "8000"}`);
+});
---
+++ frontend/src/App.js
@@ -0,0 +1,59 @@
+import { useEffect, useState } from "react";
+import { CircularProgress } from "@mui/material";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { auth } from "./config/firebase";
+import routes from "./config/routes";
+import Center from "./components/utils/Center";
+import AuthChecker from "./components/auth/AuthChecker";
+
+function App() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    auth.onAuthStateChanged((user) => {
+      if (user) {
+        user.getIdToken().then((idToken) => {
+          console.log(idToken);
+          localStorage.setItem("Token", idToken);
+        });
+        console.info("User detected.");
+      } else {
+        console.info("No user detected");
+      }
+      setLoading(false);
+    });
+  }, []);
+
+  if (loading)
+    return (
+      <Center>
+        <CircularProgress />
+      </Center>
+    );
+
+  return (
+    <div>
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
+        <Routes>
+          {routes.map((route, index) => (
+            <Route
+              key={index}
+              path={route.path}
+              element={
+                route.protected ? (
+                  <AuthChecker>
+                    <route.component />
+                  </AuthChecker>
+                ) : (
+                  <route.component />
+                )
+              }
+            />
+          ))}
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
